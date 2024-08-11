e96bb4eb7b47b881b60dfdae3336a7d5d887bb54
Abhinav Tanwar <tabhinav300@gmail.com>
2024-08-10 21:56:55 -0400

    url added

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
   development: {
-    backendUrl: "http://localhost:8080/api/v1/dalle",
+    backendUrl: "https://custom-design-backend-b94a7c7d977e.herokuapp.com",
   },
   production: {
     backendUrl: "https://devswag.onrender.com/api/v1/dalle",
